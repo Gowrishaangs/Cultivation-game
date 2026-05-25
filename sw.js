7134a1ab172caf2e563a981b730f98a6cdd5c898
Gowrishaangs <gowrishankarppm@gmail.com>
2026-05-25 11:47:24 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "xianxia-v2";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700;900&family=Crimson+Text:ital,wght@0,400;1,400&display=swap"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => {
+      // Cache what we can; skip failures for external fonts etc
+      return Promise.allSettled(ASSETS.map(url => c.add(url).catch(() => {})));
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === "basic") {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
